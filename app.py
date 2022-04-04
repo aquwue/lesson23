0ebdb90d94a170e6a0d2f6a41610eda22f9fe047
alexo <alexowl10@gmail.com>
2022-04-04 21:48:54 +0300

    new changes

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
+def build_query(fd, query):
+    query_items = query.split("|")
+    res = map(lambda v: v.strip(), fd)
+    print(res)
+    print(query_items)
+    for item in query_items:
+        split_item = item.split(":")
+        cmd = split_item[0]
+        if cmd == "filter":
+            arg = split_item[1]
+            res = filter(lambda v, txt=arg: txt in v, res)
+            print(res)
+        if cmd == "map":
+            arg = int(split_item[1])
+            res = map(lambda v, idx=arg: v.split("")[idx], res)
+        if cmd == "unique":
+            res = set(res)
+        if cmd == "sort":
+            arg = split_item[1]
+            reverse = arg == "desc"
+            res = sorted(res, reverse=reverse)
+        if cmd == "limit":
+            arg = int(split_item[1])
+            res = list(res)[:arg]
+
+        return res
+
+
 
 @app.route("/perform_query")
 def perform_query():
+    try:
+        query = request.args["query"]
+        filename = request.args["file_name"]
+    except KeyError:
+        raise BadRequest
+
+    file_path = os.path.join(DATA_DIR, filename)
+    if not os.path.exists(file_path):
+        return BadRequest(description=f"{filename} was not found")
+
+    with open(file_path) as fd:
+        res = build_query(fd, query)
+        content = '\n'.join(res)
+
+
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    return app.response_class(content, content_type="text/plain")
